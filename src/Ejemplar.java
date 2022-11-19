7259964b46010d0298c25c8a763d28ea794459ce
carol <carol@DESKTOP-826B610>
2022-11-19 15:49:17 -0500

    Clase Ejemplar

---
+++ src/Ejemplar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carol
+ */
+public class Ejemplar {
+        private int idEjemplar;
+    private String idLibro, estadoEjemplar;
+
+    public Ejemplar(int idEjemplar, String idLibro, String estadoEjemplar) {
+        this.idEjemplar = idEjemplar;
+        this.idLibro = idLibro;
+        this.estadoEjemplar = estadoEjemplar;
+    }
+
+}
